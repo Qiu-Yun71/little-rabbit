c5b6820313d1a256ddf4579f93cf19e75e8dbc3c
Qiu-Yun71 <3321839624@qq.com>
2026-05-14 23:00:28 +0800

    路由配置

--- little-rabbit/src/router/index.js
+++ little-rabbit/src/router/index.js
@@ -1,22 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+//解释下，一般写到login这里，会自动找文件夹内的index入口文件，但是尽量写全避免出错，只写文件夹名字实际上导入的是文件夹中的index文件
+import Login from '@/views/Login/index.vue'
+import Layout from '@/views/Layout/index.vue'
+import Home from '@/views/Home/index.vue'
+import Category from '@/views/Category/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      component: Layout,
+      //注意children内path最前面没有/，且第一个一般是默认子路由路径为空
+      children: [{
+        path: '',
+        component: Home
+      },
+      {
+        path: 'category',
+        component: Category
+      }]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+      path: '/login',
+      component: Login
+    }
+
   ],
 })
 
