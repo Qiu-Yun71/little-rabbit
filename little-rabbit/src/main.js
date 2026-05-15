c0f8964f9186c8c75b789b8af5d9742ab7ab28ff
Qiu-Yun71 <3321839624@qq.com>
2026-05-15 22:27:42 +0800

    吸顶导航（使用vue use）

--- little-rabbit/src/main.js
+++ little-rabbit/src/main.js
@@ -6,11 +6,11 @@ import router from './router'
 
 import '@/styles/common.scss'
 
-//测试接口函数
-import { getCategory } from './apis/testapi'
-getCategory().then(res => {
-    console.log(res)
-})
+// //测试接口函数
+// import { getCategory } from './apis/testapi'
+// getCategory().then(res => {
+//     console.log(res)
+// })
 
 const app = createApp(App)
 
--- little-rabbit/src/views/Layout/index.vue
+++ little-rabbit/src/views/Layout/index.vue
@@ -2,12 +2,14 @@
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutHeaderFixed from './components/LayoutHeaderFixed.vue';
 </script>
 
 <template>
 <!-- 引入阿里字符化图标 -->
   <link rel="stylesheet" href="//at.alicdn.com/t/c/font_5178745_fmz1a8iuevi.css">
 
+  <LayoutHeaderFixed/>
   <LayoutNav />
   <LayoutHeader />
   <RouterView />
