54ed35f21c9fddab7334357e8abf9362b56590b5
Qiu-Yun71 <3321839624@qq.com>
2026-06-02 21:44:07 +0800

    蒙版方块跟随鼠标移动

--- little-rabbit/src/components/ImageView/index.vue
+++ little-rabbit/src/components/ImageView/index.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { ref } from 'vue';
+import { useMouseInElement } from '@vueuse/core';
+import { watch } from 'vue';
 // 图片列表
 const imageList = [
   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -15,6 +17,28 @@ function enterhandle(i) {
 }
 
 
+const target = ref(null)
+const { elementX, elementY, isOutSide } = useMouseInElement(target)
+const left = ref(0)
+const top = ref(0)
+watch([elementX, elementY, isOutSide], () => {
+ // 横向
+  if (elementX.value > 100 && elementX.value < 300) {
+    left.value = elementX.value - 100
+  }
+  // 纵向
+  if (elementY.value > 100 && elementY.value < 300) {
+    top.value = elementY.value - 100
+  }
+
+  // 处理边界
+  if (elementX.value > 300) { left.value = 200 }
+  if (elementX.value < 100) { left.value = 0 }
+
+  if (elementY.value > 300) { top.value = 200 }
+  if (elementY.value < 100) { top.value = 0 }
+
+})
 </script>
 
 
@@ -24,7 +48,7 @@ function enterhandle(i) {
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
 
     </div>
 
