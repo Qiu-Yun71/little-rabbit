79f2abecf66e6141b5aace2f36f25c6e65aab981
Qiu-Yun71 <3321839624@qq.com>
2026-06-17 17:51:54 +0800

    支付模块（支付存在系统繁忙问题，直接跳转解决）

--- little-rabbit/src/views/Pay/index.vue
+++ little-rabbit/src/views/Pay/index.vue
@@ -13,6 +13,13 @@ const getOrder = async () => {
 onMounted(() => {
     getOrder()
 })
+
+
+// 支付地址
+const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net/'
+const backURL = 'http://127.0.0.1:5173/paycallback'
+const redirectUrl = encodeURIComponent(backURL)
+const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redirectUrl}`
 </script>
 
 
